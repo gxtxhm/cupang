b39eefa5d0cc13aa993c37695fd7b8a22ad956e8
gxtxhm <macdollyh@naver.com>
2021-05-09 18:27:31 +0900

    로그인페이지완료 + app.js로 로그인창띄우기구현

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 let express = require("express"); //express를 요청하면 함수값이 반환된다. 
 let app = express(); //이런식으로 사용한다.
+let fs = require('fs');//파일 입출력에 사용
+let path = require('path');
 
+app.use(express.static(path.join(__dirname,'public')));
+//*****************html, css, js, 이미지 파일 등은 public폴더 안에 있는 폴더에 각각 넣고 경로지정하면 될듯.****************
 
 //get은 라우터이다. 라우터는 들어온 요청이 길을 찾게 해주는 것이다.
 //길을 찾게해주는 과정을 라우팅이라고 부른다. 
 //밑에 코드는 '/'이 url로 접속한다면 res.send를 실행하겠다는 뜻이다. 응답하겠다는뜻이다.
 app.get('/', (req, res)=>{
-
-    res.send('Hello Home Page');
+    res.sendFile(__dirname+"/public/html/login.html");
 });
 
+//로그인이니까 post방식으로 바꿔야함 html파일도 
 app.get('/login',(req,res)=>{
-    res.send("<p>i'm surrounded p tag!</p>");//이런식으로 태그로 생성할 수도 있다.
+    res.send("<p>login Success!</p>");//이런식으로 태그로 생성할 수도 있다.
 })
 app.listen(3000, ()=> console.log('3000 port!'));//3000포트를 리슨하게 하는것이다.
